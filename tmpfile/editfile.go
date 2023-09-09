d056b7b0b41ef59d97215fb8d49954d52ea2f2a5
dpp0900 <_dpp@kakao.com>
2023-09-09 15:41:26 +0900

    GPT가 짜쭘

--- tmpfile/editfile.go
+++ tmpfile/editfile.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type Header struct {
-	signiture string   //.GR4PE
-	idx       []uint64 //IDXn.0xFFFFFFFFFFFF
+	signiture []byte //.GR4PE
+	idx       []byte //IDXn.0xFFFFFFFFFFFF
 }
 
 type deleteddData struct {
@@ -16,6 +18,11 @@ type deleteddData struct {
 	data []byte //data
 }
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 func tt() {
 	// 입력 파일 이름과 출력 파일 이름을 지정합니다.
 	inputFileName := "1080p가세혁-해킹시연동영상공모전.mp4"
@@ -54,6 +61,68 @@ func tt() {
 	fmt.Println("파일 처리 완료")
 }
 
-func genHeader(deleteddData deleteddData) Header {
-	return Header{}
+func padidx(idx uint64) []byte {
+	var ret = []byte(".0x" + strings.Repeat("0", (12-len(strconv.FormatUint(idx, 16)))) + strconv.FormatUint(idx, 16))
+	return ret
+}
+
+func genHeader(deletedData []deleteddData) Header {
+	var idx []byte
+	var cnt int
+	for _, v := range deletedData {
+		idx = append(idx, []byte("IDX"+strconv.Itoa(cnt)+string(padidx(v.idx)))...)
+		cnt++
+	}
+	fmt.Println(idx)
+	return Header{
+		signiture: []byte(".GR4PE"),
+		idx:       idx,
+	}
+}
+
+func readByte(filename string) ([]byte, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+	fileSize := fileInfo.Size()
+	fileBytes := make([]byte, fileSize)
+	bytesRead := 0
+	for bytesRead < int(fileSize) {
+		n, err := file.Read(fileBytes[bytesRead:])
+		if err != nil && err != io.EOF {
+			return nil, err
+		}
+		if n == 0 {
+			break
+		}
+		bytesRead += n
+	}
+
+	return fileBytes, nil
+}
+
+func main() {
+	inputFileName := "tmpfile/in/test"
+	buf, err := readByte(inputFileName)
+	check(err)
+	fmt.Print(genHeader([]deleteddData{
+		{
+			idx:  0x000000000000,
+			data: buf,
+		},
+		{
+			idx:  0x000000000001,
+			data: []byte("test2"),
+		},
+		{
+			idx:  0x000000000002,
+			data: []byte("test3"),
+		},
+	}))
 }
